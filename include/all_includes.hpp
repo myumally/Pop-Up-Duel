e3344959b64b00884f372b1a994b4621742bf8f4
Domi <domitillevale@gmail.com>
2025-02-08 22:51:38 +0100

    header file for all includes needed in main and for tests

---
+++ include/all_includes.hpp
@@ -0,0 +1,12 @@
+#ifndef all_includes_hpp
+#define all_includes_hpp
+
+#include "catch.hpp"
+
+#include "Card.hpp"
+#include "../src/Card.cpp"
+
+#include "Player.hpp"
+#include "../src/Player.cpp"
+
+#endif
