3991d0921ba55643f4b5582fb477419ab68fbf2b
Domi <domitillevale@gmail.com>
2025-02-14 01:11:27 +0100

    Deck Creation Menu nearly finished

--- include/DeckCreationMenu.hpp
+++ include/DeckCreationMenu.hpp
@@ -1,3 +1,7 @@
+#ifndef deck_creation_menu__hpp
+#define deck_creation_menu__hpp
+
+#include "../include/all_includes.hpp"
 #include <vector>
 #include <string>
 #include <SFML/Graphics.hpp>
@@ -8,12 +12,13 @@
 class DeckCreationMenu {
 
   std::vector<sf::Sprite> sprites;
+  Player* player;
   float scrollOffset = 0.f;
   float scrollSpeed = 20.f;
 
   public:
-    DeckCreationMenu(std::vector<sf::Sprite> Sprites);
-    void run();
+    DeckCreationMenu(std::vector<sf::Sprite> Sprites, Player* p);
+    void run(const std::array<Card*, 122>& AllCards);
 };
 
-
+#endif
--- src/DeckCreationMenu.cpp
+++ src/DeckCreationMenu.cpp
@@ -1,12 +1,15 @@
 #include "../include/DeckCreationMenu.hpp"
 
-DeckCreationMenu::DeckCreationMenu(std::vector<sf::Sprite> Sprites){
+DeckCreationMenu::DeckCreationMenu(std::vector<sf::Sprite> Sprites, Player* p){
   sprites = Sprites;
+  player = p;
 }
 
-void DeckCreationMenu::run() {
+void DeckCreationMenu::run(const std::array<Card*, 122>& AllCards) {
 
   bool RUN = true;
+
+  std::list<Card*> deck = player->getDeck();
 
   // Get Screen Dimension
   sf::VideoMode desktopMode = sf::VideoMode::getDesktopMode();
@@ -19,6 +22,10 @@ void DeckCreationMenu::run() {
   BehindCard.setSize(sf::Vector2f(950.f, 2000.f));
   BehindCard.setFillColor(sf::Color(200, 200, 180));
   BehindCard.setPosition(550.f, 0);
+
+  int cardCount = 0;
+
+  std::vector<int> SelectedCards;
 
   // mouse's position and state
   int x = 0;
@@ -76,8 +83,20 @@ void DeckCreationMenu::run() {
           id_mouse_card = i + 1;
           std::cout << "La souris est sur l'image " << id_mouse_card << std::endl;
 
+          if (std::find(SelectedCards.begin(), SelectedCards.end(), id_mouse_card) != SelectedCards.end()){
+            std::cout << "remove " << id_mouse_card << std::endl;
+            player->removeCard(id_mouse_card);
+            SelectedCards.erase(std::remove(SelectedCards.begin(), SelectedCards.end(), id_mouse_card), SelectedCards.end());
+          } 
+
+          else if(!player->isDeckComplete()) {
+            std::cout << "add " << id_mouse_card << std::endl;
+            player->addCard(id_mouse_card, AllCards); 
+            SelectedCards.push_back(id_mouse_card);
+          } 
         }
       }
+      deck = player->getDeck();
     }
 
     // render
@@ -87,8 +106,19 @@ void DeckCreationMenu::run() {
     window.draw(BehindCard);
 
     for (int i = 0; i < 122; ++i){
-      sprites[i].setPosition(600.f + (i%3) * 300.f, ((i/3) * 400.f) + scrollOffset);
-      window.draw(sprites[i]); 
+      if (!(std::find(SelectedCards.begin(), SelectedCards.end(), (i + 1)) != SelectedCards.end())){
+        sprites[i].setScale(2.0f, 2.0f);
+        sprites[i].setPosition(600.f + (i%3) * 300.f, ((i/3) * 400.f) + scrollOffset);
+      }
+      window.draw(sprites[i]);
+    }
+
+    cardCount = 0;
+    for (Card* c : deck){
+      ++cardCount;
+      sprites[c->getId() - 1].setScale(1.f, 1.f);
+      sprites[c->getId() - 1].setPosition(55.f + ((cardCount - 1)%3) * 150.f, (((cardCount - 1)/3) * 200.f) );
+      window.draw(sprites[c->getId() - 1]);
     }
 
     window.display();
